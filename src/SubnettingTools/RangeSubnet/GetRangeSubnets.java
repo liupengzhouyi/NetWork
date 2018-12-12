c52b0530ddb6df04d1e17bf8cf4a46cecebb7251
liupeng <LIUPENG.0@outlook.com>
2018-12-13 01:11:02 +0800

    三个二进制IP地址 转化为 范围类

--- src/SubnettingTools/RangeSubnet/GetRangeSubnets.java
+++ src/SubnettingTools/RangeSubnet/GetRangeSubnets.java
@@ -1,7 +1,10 @@
 package SubnettingTools.RangeSubnet;
 
 import SubnettingTools.IP.BinaryIPAddress;
+import SubnettingTools.IP.IPAddress;
+import SubnettingTools.IP.SubnetMask;
 import SubnettingTools.Subnet.RangeSubnet;
+import SubnettingTools.Tools.IPAddressBinaryToDecimal;
 
 public class GetRangeSubnets {
 
@@ -11,24 +14,141 @@ public class GetRangeSubnets {
 
     private String broadcastBinIPAddress = null;
 
-    private RangeSubnet rangeSubnet = null;
+    private RangeSubnets rangeSubnets = null;
 
     private BinaryIPAddress binaryIPAddress = null;
 
+    private IPAddress startIPAddress = null;
+
+    private IPAddress endIPAddress = null;
+
+    private IPAddress broadcastIPAddres = null;
     /**
      * 构造函数
      * @param endBinIPAddress
      * @param startBinIPAddress
      * @param broadcastBinIPAddress
      */
-    public GetRangeSubnets(String endBinIPAddress, String startBinIPAddress, String broadcastBinIPAddress) {
-        this.endBinIPAddress = endBinIPAddress;
-        this.startBinIPAddress = startBinIPAddress;
-        this.broadcastBinIPAddress = broadcastBinIPAddress;
+    public GetRangeSubnets(String startBinIPAddress, String endBinIPAddress, String broadcastBinIPAddress) {
+        //初始化
+        this.init();
+        //获取值
+        this.setStartBinIPAddress(startBinIPAddress);
+        this.setEndBinIPAddress(endBinIPAddress);
+        this.setBroadcastBinIPAddress(broadcastBinIPAddress);
+        //设置开始IP
+        this.createStartIPAddress();
+        //设置结束IP
+        this.createEndIPAddress();
+        //设置广播IP
+        this.createBroadcastIPAddress();
+        //设置广播范围
+        this.createRangeSubnets();
     }
 
+    /**
+     * 初始化
+     */
     public void init() {
+        this.binaryIPAddress = new BinaryIPAddress("", "", "", "");
+        this.rangeSubnets = new RangeSubnets(new IPAddress(0, 0, 0, 0),
+                                             new IPAddress(0, 0, 0, 0),
+                                             new IPAddress(0, 0, 0, 0));
+        this.startBinIPAddress = new String();
+        this.endBinIPAddress = new String();
+        this.broadcastBinIPAddress = new String();
 
+        this.startIPAddress = new IPAddress(0, 0, 0, 0);
+        this.endIPAddress = new IPAddress(0, 0, 0, 0);
+        this.broadcastIPAddres = new IPAddress(0, 0, 0, 0);
+    }
+
+    /**
+     * 设置开始IP
+     */
+    public void createStartIPAddress() {
+        String[] strings = new String[4];
+        int number = this.getStartBinIPAddress().length();
+        int j = 0;
+        int k = 0;
+        strings[k] = "";
+        for (int i=0;i<number;i++) {
+            j++;
+            strings[k] = strings[k] + this.getStartBinIPAddress().charAt(i);
+            if (j == 8) {
+                j = 0;
+                //System.out.println(strings[k]);
+                k = k + 1;
+                i = i + 1;
+                if (k < 4) {
+                    strings[k] = "";
+                }
+            }
+        }
+        BinaryIPAddress binaryIPAddress = new BinaryIPAddress(strings[0], strings[1], strings[2], strings[3]);
+        IPAddressBinaryToDecimal ipAddressBinaryToDecimal = new IPAddressBinaryToDecimal(binaryIPAddress);
+        this.setStartIPAddress(ipAddressBinaryToDecimal.getIpAddress());
+    }
+
+    /**
+     * 设置结束IP
+     */
+    public void createEndIPAddress() {
+        String[] strings = new String[4];
+        int number = this.getEndBinIPAddress().length();
+        int j = 0;
+        int k = 0;
+        strings[k] = "";
+        for (int i=0;i<number;i++) {
+            j++;
+            strings[k] = strings[k] + this.getEndBinIPAddress().charAt(i);
+            if (j == 8) {
+                j = 0;
+                //System.out.println(strings[k]);
+                k = k + 1;
+                i = i + 1;
+                if (k < 4) {
+                    strings[k] = "";
+                }
+            }
+        }
+        BinaryIPAddress binaryIPAddress = new BinaryIPAddress(strings[0], strings[1], strings[2], strings[3]);
+        IPAddressBinaryToDecimal ipAddressBinaryToDecimal = new IPAddressBinaryToDecimal(binaryIPAddress);
+        this.setEndIPAddress(ipAddressBinaryToDecimal.getIpAddress());
+    }
+
+    /**
+     * 设置广播IP
+     */
+    public void createBroadcastIPAddress() {
+        String[] strings = new String[4];
+        int number = this.getBroadcastBinIPAddress().length();
+        int j = 0;
+        int k = 0;
+        strings[k] = "";
+        for (int i=0;i<number;i++) {
+            j++;
+            strings[k] = strings[k] + this.getBroadcastBinIPAddress().charAt(i);
+            if (j == 8) {
+                j = 0;
+                //System.out.println(strings[k]);
+                k = k + 1;
+                i = i + 1;
+                if (k < 4) {
+                    strings[k] = "";
+                }
+            }
+        }
+        BinaryIPAddress binaryIPAddress = new BinaryIPAddress(strings[0], strings[1], strings[2], strings[3]);
+        IPAddressBinaryToDecimal ipAddressBinaryToDecimal = new IPAddressBinaryToDecimal(binaryIPAddress);
+        this.setBroadcastIPAddres(ipAddressBinaryToDecimal.getIpAddress());
+    }
+
+    /**
+     * 设置广播范围
+     */
+    public void createRangeSubnets() {
+        this.setRangeSubnets(new RangeSubnets(this.getStartIPAddress(), this.getEndIPAddress(), this.getBroadcastIPAddres()));
     }
 
     public String getEndBinIPAddress() {
@@ -55,12 +175,12 @@ public class GetRangeSubnets {
         this.broadcastBinIPAddress = broadcastBinIPAddress;
     }
 
-    public RangeSubnet getRangeSubnet() {
-        return rangeSubnet;
+    public RangeSubnets getRangeSubnets() {
+        return rangeSubnets;
     }
 
-    public void setRangeSubnet(RangeSubnet rangeSubnet) {
-        this.rangeSubnet = rangeSubnet;
+    public void setRangeSubnets(RangeSubnets rangeSubnets) {
+        this.rangeSubnets = rangeSubnets;
     }
 
     public BinaryIPAddress getBinaryIPAddress() {
@@ -70,4 +190,42 @@ public class GetRangeSubnets {
     public void setBinaryIPAddress(BinaryIPAddress binaryIPAddress) {
         this.binaryIPAddress = binaryIPAddress;
     }
+
+    public IPAddress getStartIPAddress() {
+        return startIPAddress;
+    }
+
+    public void setStartIPAddress(IPAddress startIPAddress) {
+        this.startIPAddress = startIPAddress;
+    }
+
+    public IPAddress getEndIPAddress() {
+        return endIPAddress;
+    }
+
+    public void setEndIPAddress(IPAddress endIPAddress) {
+        this.endIPAddress = endIPAddress;
+    }
+
+    public IPAddress getBroadcastIPAddres() {
+        return broadcastIPAddres;
+    }
+
+    public void setBroadcastIPAddres(IPAddress broadcastIPAddres) {
+        this.broadcastIPAddres = broadcastIPAddres;
+    }
+
+    public static void main(String[] args) {
+        GetRangeSubnets getRangeSubnets = new GetRangeSubnets("11000000.10011100.01111011.11000000",
+                "11000000.10011100.01111011.11011110", "11000000.10011100.01111011.11011111");
+        System.out.println(getRangeSubnets.getStartBinIPAddress());
+        System.out.println(getRangeSubnets.getEndBinIPAddress());
+        System.out.println(getRangeSubnets.getBroadcastBinIPAddress());
+        System.out.println(getRangeSubnets.getStartIPAddress().getIPAddress());
+        System.out.println(getRangeSubnets.getEndIPAddress().getIPAddress());
+        System.out.println(getRangeSubnets.getBroadcastIPAddres().getIPAddress());
+        System.out.println(getRangeSubnets.getRangeSubnets().getStartIP().getIPAddress());
+    }
 }
+
+
